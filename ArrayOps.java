595e14ad9fef80021be218c18ec6a756f9ded98c
Yair Hazan <yair.hazan@post.runi.ac.il>
2024-01-10 14:20:30 +0200

    first try

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,87 @@
 public class ArrayOps {
     public static void main(String[] args) {
+        System.out.println(isSorted(new int[] {7, 5, 4, 3, -12}));
+        System.out.println(isSorted(new int[] {1, 2, 3}));
+        System.out.println(isSorted(new int[] {1, -2, 3}));
+        System.out.println(isSorted(new int[] {1, 1, 500}));
+        System.out.println(isSorted(new int[] {1, 3, 2}));
         
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
+        boolean flag = false;
+        for (int i = 0; i <= array.length; i++) {
+            //System.out.println("finding: " + i);
+            for (int j = 0; j < array.length; j++) {
+                flag = false;
+                //System.out.println("Array in j: " + array[j]);
+                if(array[j] == i){
+                    flag = true;
+                    break;
+                }
+                
+            }
+            if (!flag) {
+                    return i;
+                }
+        }
         return -1;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int secondmax = 0, max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max){
+                secondmax = max;
+                max = array[i];
+            }else if (array[i] > secondmax) {
+                secondmax = array[i];
+            }
+        }
+        return secondmax;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        boolean flag = false;
+        for (int i = 0; i < array1.length; i++) {
+            flag = false;
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                    return false;
+                }
+        }
+        return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        boolean isIncrease = true; // true for increaseing and false if decreasing
+        boolean flag = false;
+        int pointer = array[0];
+        if (array[0] > array[1]) 
+            isIncrease = false;
+        for (int i = 0; i < array.length; i++) {
+            flag = false;
+            if (array[i]>=pointer && isIncrease) {
+                flag = true;
+                pointer = array[i];
+            }
+            if (array[i]<=pointer && !isIncrease) {
+                flag = true;
+                pointer = array[i];
+            }
+            if (!flag) {
+                break;
+            }
+            if (i == array.length - 1) {
+                return true;
+            }
+        }
+        return false;        
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,116 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+    
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String S = "";
+        for (int i = 0; i < string.length(); i++) {
+            if ("aeiou".indexOf(string.charAt(i)) != -1) { //is the char a low case vowl
+                S += (char)(string.charAt(i) - 32);
+            }else if (string.charAt(i) <= 90 && string.charAt(i) >= 65) {
+                S += (char)(string.charAt(i) + 32);
+            }else{
+                S += string.charAt(i); 
+            }
+        }
+        return S;
     }
 
     public static String camelCase (String string) {
         // Write your code here:
-        return "";
+        string = dropSpacesStart(string);
+        string = lowercaseall(string);
+        string = capitalFirstInWord(string);
+        string = dropSpaces(string);
+        return string;
+    }
+
+    private static String dropSpacesStart(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) != ' ') {
+                return string.substring(i);
+            }
+        }
+        return null;
+    }
+
+    private static String capitalFirstInWord(String string) {
+        String S = "";
+        S += lowercase(string.charAt(0));
+        for (int i = 1; i < string.length(); i++) {
+            if (string.charAt(i) == ' ') {
+                S += string.charAt(i);
+            }else if (string.charAt(i-1) == ' ') {
+                S += uppercase(string.charAt(i));
+            }else{
+                S += string.charAt(i);
+            }
+        }
+        return S;
+    }
+
+    private static char uppercase(char charAt) {
+        if (charAt >= 97 && charAt <= 122) {
+            return (char)(charAt - 32);
+        }
+        return charAt;    
+        
+    }
+
+    private static char lowercase(char charAt) {
+        if (charAt >= 65 && charAt <= 90) {
+            return (char)(charAt + 32);
+        }
+        return charAt;
+    }
+
+    private static String lowercaseall(String string) {
+        String S = "";
+        for (int i = 0; i < string.length(); i++) {
+            S += lowercase(string.charAt(i));
+        }
+        return S;
+    }
+
+    private static String dropSpaces(String string) {
+        String S = "";
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) != ' ') {
+                S += string.charAt(i);
+            }
+        }
+        return S;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        //**countChar();
+        //creat arrayin this length;
+        //for that finds the char and appends i to the array
+        //**return new int[1];
+        int chrCounter = countChar(string,chr);
+        int [] array = new int[chrCounter];
+        int j = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                array[j] = i;
+                j++;
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        return array;
+    }
+
+    private static int countChar(String string, char chr) {
+        int counter = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
